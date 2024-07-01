efc62ceea5b12339ca571f4b1439065dc0f301c5
VTHMgNPipola <12537499+VTHMgNPipola@users.noreply.github.com>
2024-07-01 15:12:54 -0300

    Converted to modular application.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+open module com.vthmgnpipola.kibsymbolbuilder {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires org.apache.commons.csv;
+    requires org.slf4j;
+}
